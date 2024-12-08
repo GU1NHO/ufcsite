b93137a98c4f9c16b8101b18efe2d4e62cbf2b37
GU1NHO <thiagorosacarvalho@usp.br>
2024-12-08 18:28:12 -0300

    Quinta tentativa render

--- build.sh
+++ build.sh
@@ -5,3 +5,20 @@ set -o errexit
 pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
+
+# Execute as migrações antes de qualquer outra coisa
+python manage.py migrate
+
+# Crie o superusuário se não existir
+python manage.py shell << EOF
+import os
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+if not User.objects.filter(username=os.environ.get("DJANGO_SUPERUSER_USERNAME", "admin")).exists():
+    User.objects.create_superuser(
+        os.environ.get("DJANGO_SUPERUSER_USERNAME", "admin"),
+        os.environ.get("DJANGO_SUPERUSER_EMAIL", "admin@example.com"),
+        os.environ.get("DJANGO_SUPERUSER_PASSWORD", "password")
+    )
+EOF
